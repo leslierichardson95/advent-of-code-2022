7d37a205446f6de8cdf85a7b59ff235b06076f5d
Leslie Richardson <lerich@microsoft.com>
2022-12-03 16:36:30 -0800

    Days 2 & 3 Complete!

---
+++ Day2/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rounds = File.ReadAllLines("C:/Users/lerich/OneDrive - Microsoft/source/advent-of-code-2022/Day2/input.txt");
+            Console.WriteLine("Part 1: " + Part1(rounds));
+            Console.WriteLine("Part 2: " + Part2(rounds));
+        }
+
+        static int Part1(string[] rounds)
+        {
+            int totalScore = 0;
+
+            // A = rock, B = paper, C = scissors
+            // X(1) = rock, Y(2) = paper, Z(3) = scissors
+            foreach (string round in rounds)
+            {
+                if (round[0] == 'A') // rock
+                {
+                    if (round[2] == 'X') totalScore += (1 + 3);
+                    else if (round[2] == 'Y') totalScore += (2 + 6);
+                    else totalScore += (3 + 0);
+                }
+                else if (round[0] == 'B') // paper
+                {
+                    if (round[2] == 'X') totalScore += (1 + 0);
+                    else if (round[2] == 'Y') totalScore += (2 + 3);
+                    else totalScore += (3 + 6);
+                }
+                else // scissors
+                {
+                    if (round[2] == 'X') totalScore += (1 + 6);
+                    else if (round[2] == 'Y') totalScore += (2 + 0);
+                    else totalScore += (3 + 3);
+                }
+            }
+            return totalScore;
+        }
+
+        static int Part2(string[] rounds)
+        {
+            int totalScore = 0;
+
+            // A = rock, B = paper, C = scissors
+            // X(1) = rock, Y(2) = paper, Z(3) = scissors
+            foreach (string round in rounds)
+            {
+                if (round[0] == 'A') // rock
+                {
+                    if (round[2] == 'X') totalScore += (3 + 0);
+                    else if (round[2] == 'Y') totalScore += (1 + 3);
+                    else totalScore += (2 + 6);
+                }
+                else if (round[0] == 'B') // paper
+                {
+                    if (round[2] == 'X') totalScore += (1 + 0);
+                    else if (round[2] == 'Y') totalScore += (2 + 3);
+                    else totalScore += (3 + 6);
+                }
+                else // scissors
+                {
+                    if (round[2] == 'X') totalScore += (2 + 0);
+                    else if (round[2] == 'Y') totalScore += (3 + 3);
+                    else totalScore += (1 + 6);
+                }
+            }
+
+            return totalScore;
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rucksacks = File.ReadAllLines("C:/Users/lerich/OneDrive - Microsoft/source/advent-of-code-2022/Day3/input.txt");
+            Console.WriteLine("Part 1: " + Part1(rucksacks));
+            Console.WriteLine("Part 2: " + Part2(rucksacks));
+        }
+
+        static int Part1(string[] rucksacks)
+        {
+            int sum = 0;
+
+            foreach (string rucksack in rucksacks)
+            {
+                int halfIndex = rucksack.Length / 2;
+                string compartment1 = rucksack[0..halfIndex];
+                string compartment2 = rucksack[halfIndex..rucksack.Length];
+
+                foreach (char item in compartment1)
+                {
+                    if (compartment2.Contains(item))
+                    {
+                        if (!Char.IsUpper(item)) sum += (item - 'a' + 1);
+                        else sum += (item - 'A' + 27);
+                        break;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        static int Part2(string[] rucksacks)
+        {
+            int sum = 0;
+            for (int i = 0; i < rucksacks.Length; i += 3)
+            {
+                string rucksack1 = rucksacks[i];
+                string rucksack2 = rucksacks[i + 1];
+                string rucksack3 = rucksacks[i + 2];
+
+                foreach (char item in rucksack1)
+                {
+                    if (rucksack2.Contains(item) && rucksack3.Contains(item))
+                    {
+                        if (!Char.IsUpper(item)) sum += (item - 'a' + 1);
+                        else sum += (item - 'A' + 27);
+                        break;
+                    }
+                }
+            }   
+
+            return sum;
+        }
+    }
+}
